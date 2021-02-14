37fae525fd6fe584ce175afd1eda67c940bd7820
Alexander Lester <alexander480@gmail.com>
2021-02-14 02:31:01 -0500

    Initial Commit

---
+++ HealthWidgets/HealthWidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthWidgetsApp.swift
+//  HealthWidgets
+//
+//  Created by Alexander Lester on 2/14/21.
+//
+
+import SwiftUI
+
+@main
+struct HealthWidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
